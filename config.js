b8da8e08a22bf6d9b3156bff76b429260ba90c05
Goldy Mariz Lunesa <gmlunesa@gmail.com>
2018-05-25 17:31:01 +0800

    Cleaned up config.js

--- config.js
+++ config.js
@@ -8,9 +8,6 @@ jasmine.loadConfig({
     spec_dir: 'spec',
     spec_files: [
         'StudySpec.js'
-        // 'requests/*/*[sS]pec.js',
-        // 'utils/**/*[sS]pec.js'
-        // 'jasmine_examples/**/*[sS]pec.js'*'
     ],
     helpers: [
         'helpers/**/*.js'
@@ -25,16 +22,5 @@ jasmine.onComplete(function(passed) {
         console.log('At least one spec has failed');
     }
 });
-
-// jasmine.configureDefaultReporter({
-//     // The timer passed to the reporter will determine the mechanism for seeing how long the suite takes to run.
-//     timer: new jasmine.jasmine.Timer(),
-//     // The print function passed the reporter will be called to print its results.
-//     print: function() {
-//         process.stdout.write(arguments);
-//     },
-//     // showColors determines whether or not the reporter should use ANSI color codes.
-//     showColors: true
-// });
 
 jasmine.execute()
