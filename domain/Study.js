e50cf9b45a9c35752fba4d3a51bf6f626bc028e0
Goldy Mariz Lunesa <gmlunesa@gmail.com>
2018-05-25 17:44:47 +0800

    More functionalities

--- domain/Study.js
+++ domain/Study.js
@@ -12,9 +12,32 @@ function Study() {
     }
 
     var File = function(filename) {
+    	var comments =[]
         return {
-            filename: filename
+            filename: filename,
+            comments: comments
+        
         };
+    }
+    
+    var Comment = function(username, comment){
+    	return{
+    		username: username,
+    		comment:comment
+    	}
+
+    }
+
+    this.addComment = function (email, comment, filename){
+    	var newComment = new Comment(email, comment);
+    	if (this.getFile(email,filename).comments){
+    		var newCommentSize = this.getFile(email,filename).comments.length;
+    	} else {
+    		var newCommentSize = 0;
+    	}
+    	
+    	this.getFile(email,filename).comments[newCommentSize] = newComment;
+    	//console.log(comments)
     }
 
     this.addUser = function(name, email) {
@@ -22,6 +45,7 @@ function Study() {
     	users[users.length] = newUser;
     	this.length += 1;
     }
+
 
     this.addFile = function(email, filename) {
     	var newFile = new File(filename);
@@ -34,6 +58,7 @@ function Study() {
     	this.getUser(email).files[newFileSize] = newFile;
     	//this.getUser(email).albums.length++;
     }
+    
 
     this.getUser = function(email) {
     	 for (var i = 0; i < users.length; i++) {
@@ -50,6 +75,17 @@ function Study() {
     	for (var i = 0; i <= email.files.length; i++) {
     		if (email.files[i].filename === filename) {
     			return email.files[i];
+    		}
+    	}
+    	return -1;
+    }
+
+    this.getComment = function(username, filename, comment) {
+    	file = this.getFile(username,filename);
+
+    	for (var i = 0; i <= file.comments.length; i++) {
+    		if (file.comments[i].comment === comment) {
+    			return file.comments[i];
     		}
     	}
     	return -1;
